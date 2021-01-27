5e2dcb99e24ddab7afbf73058243584a909750aa
Brahianf <Brahianf@unicauca.edu.co>
2021-01-27 18:35:11 -0500

    [ADD] render list subCategory sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -13,11 +13,34 @@
     );
   };
 
+  function subCategoryItemTemplate(subCategory){
+    return (
+     `
+     <li class="playlistFriends-item" data-id=${subCategory.subcategory_id} data-name=${subCategory.name} data-nicename=${subCategory.nicename}>
+       <span>
+       • ${subCategory.name}
+       </span>
+     </li>
+     `
+   );
+  };
+
   function renderCategoryList(listCategory, $playlistFriends) {
     // Show list Category in Sidebar Play list
     listCategory.forEach((category) => {
       const HTMLString = categoryItemtemplate(category);
       $playlistFriends.innerHTML += HTMLString;
+    });
+  };
+
+  async function renderSubCategoryList($playlistFriendsItem){
+    $playlistFriendsItem.forEach(async (Category) => {
+      const listSubCategory =  await cacheSubCategoryItems(Category.dataset.id);
+      const $playlistFriendsubItem = Category.querySelector('ul.playlistFriends-subItem');
+      listSubCategory.forEach((subCategory) => {
+        const HTMLString = subCategoryItemTemplate(subCategory);
+        $playlistFriendsubItem.innerHTML += HTMLString
+      });
     });
   };
 
@@ -48,9 +71,23 @@
     return data;
   };
 
+  async function cacheSubCategoryItems(id){
+    const listName=`${id}items`;
+    const cacheList = window.localStorage.getItem('listName');
+    if(cacheList){
+      return JSON.parse(window.localStorage.getItem('listName'));
+    }
+    const data =  await getData(`${BASE_API}?get_subcategories_by_category_ID=${id}`);
+    window.localStorage.setItem(listName, JSON.stringify(data));
+    return data;
+  };
+
   // List Category of Api or cache, and Render in Sidebar
   const listCategory = await cacheCategory('list-category');
   const $playlistFriends = document.querySelector('#playlistFriends');
   renderCategoryList(listCategory, $playlistFriends);
+
+  const $playlistFriendsItem = document.querySelectorAll('li.playlistFriends-item');
+  renderSubCategoryList($playlistFriendsItem);
 
 })();
